10ae477d471a5e0c92f6c9d8c0981705456a56ce
4pers0n <zhw2513953778@outlook.com>
2022-08-08 22:36:36 -0700

    generate vertexbuffer for drawing a triangle

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -21,7 +22,36 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     /* Initialize GLEW */
-    glewInit();
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Error when initializing GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    // the actual data we put into the vertex buffer
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    // create a buffer, buffer_id is the output parameter
+    unsigned int buffer_id;
+    glGenBuffers(1, &buffer_id);
+    if (buffer_id == GL_INVALID_VALUE) {
+        std::cerr << "Error when generating a buffer" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // bind the buffer to an array buffer inside vram
+    // enter the state of having this buffer (no need to specify the buffer later)
+    // (imagine seleting a layer in Photoshop)
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+    // specify the data(we can specify the size first)
+    // check the docs.GL for this function
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -29,11 +59,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -41,7 +67,6 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
     glfwTerminate();
     return 0;
 }
